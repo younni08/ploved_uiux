6a491160fae2a5c098d2ec6af25b89f0ea7fdfd3
younni08 <younni08@gmail.com>
2022-06-02 21:02:14 +0900

    check

---
+++ src/components/local/group.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const LocalGroup = () => {
+    return (
+        <div className="localgroup">
+            <div>
+                <span>Group neab by</span>
+                <span>See all</span>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <img src="./temp1.webp" alt="img" />
+                    </div>
+                    <div>
+                        <span>Local Group</span>
+                        <span>M 2.3k</span>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img src="./temp1.webp" alt="img" />
+                    </div>
+                    <div>
+                        <span>Local Group</span>
+                        <span>M 2.3k</span>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img src="./temp1.webp" alt="img" />
+                    </div>
+                    <div>
+                        <span>Local Group</span>
+                        <span>M 2.3k</span>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img src="./temp1.webp" alt="img" />
+                    </div>
+                    <div>
+                        <span>Local Group</span>
+                        <span>M 2.3k</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default LocalGroup;
--- src/components/local/local.tsx
+++ src/components/local/local.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import LocalGroup from "./group";
 import LocalTopNavi from "./local_topnavi";
 
 const Local = () => {
     return (
         <div className="local">
             <LocalTopNavi />
+            <LocalGroup />
 
         </div>
     )
